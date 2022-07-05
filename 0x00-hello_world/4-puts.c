1ab7459af938d473b3e3ec1ea1c7a0867af8f28b
Chukwuemeka <Chukwuemeka>
2022-07-05 09:50:08 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
-include <stdio.h>
+include < stdio.h >
 
-int main(void)                                                   
-{                                                                puts("\"Programming is like building a multilingual puzzle");    return (0);                                                      
+/**
+* main - this is the main function
+* description - print the quote
+* Return: 0 if exited properly, non-zero otherwise
+*/
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
