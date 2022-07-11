2f521fa4d7d5aad1223cc2ea4d5e33053b687f78
Chukwuemeka <Chukwuemeka>
2022-07-11 14:31:05 -0700

    Hello, Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,8 @@
 * main - prints the quote
 * Return: 0 if exited properly, non-zero otherwise
 */
-
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
