bdd450325dbf7ae59d2539d597d2cfc9159feb9a
Chukwuemeka <Chukwuemeka>
2022-07-14 19:23:24 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_array_strings/0-strcat.c
+++ 0x06-pointers_array_strings/0-strcat.c
@@ -1,23 +1,32 @@
 #include "main.h"
 
 /**
-* _strcat - concatenates two strings
-* @dest: destination.
-* @src: source.
-* Return: the pointer to dest.
+* _strcat - function commute srtings
+* @dest: param pointer to a char
+* @src: param pointer to a char
+* Return: return value of dest
 */
+
 char *_strcat(char *dest, char *src)
 {
-int len = 0, i;
+int i;
+int j;
 
-while (dest[len])
-len++;
+i = 0;
+j = 0;
 
-for (i = 0; src[i] != '\0'; i++)
+while (dest[i] != '\0')
 {
-dest[len] = src[i];
-len += 1;
+i++;
 }
-dest[len] = '\0';
+
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+
+dest[i] = '\0';
 return (dest);
 }
