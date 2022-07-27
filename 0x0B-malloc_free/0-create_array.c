035eb7539ba4b62c010811aea9edf14b1a8aca6f
Chukwuemeka <Chukwuemeka>
2022-07-27 15:29:51 -0700

    Fix betty style

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *
  * create_array - creates an array of chars.
  * @size: size of the array.
  * @c: storaged char
@@ -15,9 +14,9 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	
+
 	cr = malloc(sizeof(c) * size);
-	
+
 	if (cr == NULL)
 		return (NULL);
 
