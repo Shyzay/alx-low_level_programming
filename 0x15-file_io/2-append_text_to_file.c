9eebbc4dc1f930a9a35576d9ec44ef95be16ce97
Chukwuemeka <Chukwuemeka>
2022-09-16 20:43:19 -0700

    C - File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	int nletters;
 	int rwr;
-	
+
 	if (!filename)
 		return (-1);
 
@@ -26,7 +26,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		for (nletters = 0; text_content[nletters]; nletters++)
 			;
-		
+
 		rwr = write(fd, text_content, nletters);
 
 		if (rwr == -1)
